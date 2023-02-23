fa00bc0c886ebbfdeb75e15e540e8b879b149c1d
Kenet K Ortiz <42982324+KOrtizLedezma@users.noreply.github.com>
2023-02-23 21:55:35 +0000

    Class Stable and Horse finished and Main changed

--- Main.java
+++ Main.java
@@ -2,32 +2,33 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    //Ask the user here
-	  Stable s = new Stable();
-		System.out.println("What is the address of the stable?");
-		Scanner scanner = new Scanner(System.in);
-		s.address = scanner.nextLine();
-		do {
-			System.out.println("Do you wish to add a horse to the stable?"); 
-			char input = scanner.next().charAt(0);
-			scanner.nextLine();
-			if(input == 'y'){
-				Horse h;
-				System.out.println("What is the name of the horse?");
-				String hn = scanner.nextLine();
-				System.out.println("What is the weight of the horse?");
-				double w = scanner.nextDouble();
-				System.out.println("Enter true if the horse is tame or false if it is not.");
-				boolean t = scanner.nextBoolean();
-				h = new Horse(hn, w, t);
-				s.arrayList.add(h);
-			}//testing goodest 4
-			else{
-				break;
-			}
-		}while(true);
-		//Display the info here
-                //You code here!
-	  }
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What is the address of the stable?");
+    String address = scanner.nextLine();
+    Stable stable = new Stable(address);
+
+    while (true) {
+      System.out.println("Do you wish to add a horse to the stable?");
+      String input = scanner.nextLine();
+
+      if (input.equalsIgnoreCase("n")) {
+        break;
+      }
+
+      System.out.println("What is the name of the horse?");
+      String name = scanner.nextLine();
+
+      System.out.println("What is the weight of the horse?");
+      double weight = scanner.nextDouble();
+      scanner.nextLine(); // consume the newline character
+
+      System.out.println("Enter true if the horse is tame or false if it is not.");
+      boolean isTame = scanner.nextBoolean();
+      scanner.nextLine(); // consume the newline character
+
+      Horse horse = new Horse(name, weight, isTame);
+      stable.addHorse(horse);
+    }
+    stable.showHorses();
   }
 }
